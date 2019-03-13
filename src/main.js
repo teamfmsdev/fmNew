71619094db8226b1723df51dc3344799951c1787
DoReVo <DoReVo@github.com>
2019-03-13 20:53:18 +0800

    font awesome import

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import './plugins/bootstrap-vue'
 import App from './App.vue'
 import store from './store'
 
+import '@fortawesome/fontawesome-free/css/all.min.css'
+
 Vue.config.productionTip = false
 
 new Vue({
