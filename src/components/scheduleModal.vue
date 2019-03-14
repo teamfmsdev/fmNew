ec3876d7ad167e7b256f56f1ed0333c756ccd65b
DoReVo <DoReVo@github.com>
2019-03-14 17:32:48 +0800

    1.Custom field for action/activities 2. ajax for scheduling

--- src/components/scheduleModal.vue
+++ src/components/scheduleModal.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-modal class="modalTable" centered id="scheduleModal">
+  <b-modal ref="scheduleModal" class="modalTable" centered id="scheduleModal">
     <template slot="modal-title">Schedule job</template>
 
     <b-form-select
@@ -16,9 +16,22 @@
     />
 
     <b-table hover small class="mt-3" bordered :items="listOfDays" :fields="field">
-      <span slot="activities" slot-scope="data" v-html="data.value" />
-      <span slot="action" slot-scope="data" v-html="data.value" />
-
+      <span slot="activities" slot-scope="data" v-html="data.value"/>
+      <template v-if="data.item.total!=0" slot="activities" slot-scope="data">
+        <i class="fas fas fa-check-circle fa-lg text-primary"></i>
+        <mark style="background-color:#057953; color:white;">
+          <b v-text="data.item.completed"></b>
+        </mark>
+        <mark style="background-color:#4c4c4c; color:white;">
+          <b v-text="data.item.total"></b>
+        </mark>
+      </template>
+      <template slot="action" slot-scope="data">
+        <!-- <i class="fas fa-plus-circle"></i> -->
+        <button @click="scheduleJob(data.item,data.value)" class="btn btn-sm btn-outline-primary">
+          <i class="fas fa-plus-circle"></i>
+        </button>
+      </template>
     </b-table>
   </b-modal>
 </template>
@@ -37,7 +50,7 @@ export default {
         date: "Date",
         day: "Day",
         activities: "Activities",
-        action:"Action"
+        action: "Action"
       },
       monthList: [
         { value: "Jan", text: "January" },
@@ -62,18 +75,32 @@ export default {
       ],
       selectedMonth: dayjs(new Date()).format("MMM"),
       selectedYear: dayjs(new Date()).format("YYYY"),
-      listOfDays:[],
+      listOfDays: [],
       apiUrl: process.env.VUE_APP_API_URL
     };
   },
   computed: {
     getIsLoading: {
-      get () {
-        return this.$store.getters.getIsLoading
+      get() {
+        return this.$store.getters.getIsLoading;
       },
-      set (value) {
-        this.$store.commit('changeIsLoading', value)
+      set(value) {
+        this.$store.commit("changeIsLoading", value);
       }
+    },
+    getClickedRow() {
+      return this.$store.getters.getClickedRow;
+    },
+    getEditClicked: {
+      get () {
+        return this.$store.getters.getEditClicked
+      }
+    },
+    getFormState: {
+      get () {
+        return this.$store.getters.getFormState
+      }
+      
     }
   },
   methods: {
@@ -81,7 +108,6 @@ export default {
       console.log(this.generatedDays);
     },
     generatedDays() {
-      this.listOfDays  = [];
       let date = dayjs(
         new Date(`${this.selectedYear}/${this.selectedMonth}/1`)
       );
@@ -90,7 +116,7 @@ export default {
       );
       let listOfDays = [];
 
-      this.getIsLoading = true
+      this.getIsLoading = true;
       axios
         .get(`${this.apiUrl}biaRetrieve.php`, {
           params: {
@@ -98,51 +124,92 @@ export default {
           }
         })
         .then(({ data }) => {
+          this.listOfDays = [];
           let total;
           let completed;
           // console.log(data)
           while (date.isSame(selectedMonth, "month")) {
+            total = data.filter(element => {
+              return date.isSame(dayjs(new Date(element["date"])), "date");
+            });
 
-            total = data.filter((element)=>{
-              return date.isSame(dayjs(new Date(element['date'])),'date')
-            })
-
-            completed = data.filter((element)=>{
-              return (date.isSame(dayjs(new Date(element['date'])),'date')
-              && element['status'] == 1)
-            })
+            completed = data.filter(element => {
+              return (
+                date.isSame(dayjs(new Date(element["date"])), "date") &&
+                element["status"] == 1
+              );
+            });
 
             this.listOfDays.push({
+              fullDate: date.format("YYYY-MM-DD"),
               date: date.format("DD"),
               day: date.format("ddd"),
-              activities: total.length!=0 ? `<i  class="fas fas fa-check-circle fa-lg text-primary"></i> <mark style='background-color:#057953; color:white;'><b>${completed.length}</b></mark>
-              <mark style='background-color:#4c4c4c; color:white;'><b>${total.length}</b></mark>` : '',
-              action:`<button class='btn btn-sm btn-outline-primary fas fa-plus-square fa-sm'></button>`
+              completed: completed.length,
+              total: total.length != 0 ? total.length : 0
             });
-              // <i  class="fas fa-plus-square fa-lg text-primary"></i>
+            // <i  class="fas fa-plus-square fa-lg text-primary"></i>
             date = date.add(1, "day");
           }
-          this.getIsLoading = false
+          this.getIsLoading = false;
         })
-        .catch(({data}) => {
-          while(date.isSame(selectedMonth,'month')){
-              this.listOfDays.push({
-                date:date.format("DD"),
-                day: date.format("ddd"),
-                activities : '',
-                 action:`<button  class="btn btn-sm btn-outline-primary fas fa-plus-square fa-sm"></button>`
-              })
-              date = date.add(1,'day')
-            }
-            this.getIsLoading = false
-          
+        .catch(({ data }) => {
+          this.listOfDays = [];
+          while (date.isSame(selectedMonth, "month")) {
+            this.listOfDays.push({
+              fullDate: date.format("YYYY-MM-DD"),
+              date: date.format("DD"),
+              day: date.format("ddd"),
+              completed: "",
+              total: ""
+            });
+            date = date.add(1, "day");
+          }
+          this.getIsLoading = false;
+        });
+    },
+    scheduleJob(item, value) {
+      this.getIsLoading = true;
+
+      axios
+        .get(`${this.apiUrl}scheduleJob.php`, {
+          params: {
+            date: item.fullDate,
+            fmNo: this.getClickedRow
+          }
+        })
+        .then(({ data }) => {
+          // If success
+          console.log(data);
+          if (data==1) {
+            this.generatedDays();
+
+            this.$store.dispatch("updateJobStatus");
+
+            uiControl.displayMessage("Job scheduled");
+          } else if (data==0) {
+            uiControl.displayMessage("Failed to schedule job");
+          }
+
+          if (this.getEditClicked && this.getFormState) {
+           uiControl.editReset()
+          }
+
+          this.$store.dispatch("resetForm");
+          this.$refs.scheduleModal.hide();
+          this.getIsLoading = false;
+        })
+        .catch((response) => {
+          console.log(response)
+          this.$refs.scheduleModal.hide();
+          uiControl.displayMessage("Connection Error");
+          this.getIsLoading = false;
         });
 
       
     }
   },
   created() {
-    this.generatedDays()
+    this.generatedDays();
   }
 };
 </script>
